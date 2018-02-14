51dc8048d26fd45baefcfc37a7c077d4741f6161
Marklar <dmitri.bilyk@gmail.com>
2018-02-14 11:40:29 -0800

    oops, removed the node_modules folder from tracking

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export default class App extends React.Component {
   render() {
     return (
-      <h1 > Hello World! </h1 >
+      <h1 > Hello World! I'm a react app!</h1 >
+
     )
 
   }
